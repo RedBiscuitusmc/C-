d663a3b4695a7d35036d3068bf6e6ce3cd98a4d5
Jake <105008704+RedBiscuitusmc@users.noreply.github.com>
2023-10-19 14:25:54 -0400

    Create varibles.h

---
+++ varibles.h
@@ -0,0 +1,38 @@
+#include <string>
+
+using namespace std;
+
+void Variables();
+void ConstantExample();
+void typeDEFmethod();
+void MathOps();
+void TypeConversion();
+void MathFunctions();
+void SpacersForTerm();
+void HelloWorld();
+void UserInputIntro();
+void UserInputCaseMonths();
+void ifStatementsInfo();
+void RightAngleInfo();
+void UserInputINFO();
+void TernaryOp();
+void userInputLoadUp();
+void LaunchForLoops();
+void UserDefinedFunctions();
+void HelperHappyBirthday(string name);
+void ReturnsFunction();
+double HelperDoubleReturn(double length);
+void BakePizza();
+void BakePizza(string topping1);
+
+
+//methods that require inputs ::
+void SwitchStatements();
+void ifStatements();
+void RightAngle();
+void UserInput();
+void Logicalops();
+void StringMethods();
+void LaunchWhileLoops();
+void LaunchDoWhile();
+void NestedForLoops();
